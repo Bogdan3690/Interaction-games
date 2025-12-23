e33ea8d2adb61838efac53bf48a64b8c9fe66a20
Bogdan3690 <dblevko@gmail.com>
2025-12-23 13:09:08 -0700

    add js check year

---
+++ js/cheak-year.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.js-cheak-year-form')
+const text = document.querySelector('.js-cheak-year-text')
+
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(event){
+    event.preventDefault()
+    console.log(event.target.elements.yearInput.value);
+    const form = event.target
+    const value = form.elements.yearInput.value
+    console.log(typeof value);
+    const yearValue = Number.parseInt(value)
+    if (Number.isNaN(yearValue)) {
+        text.textContent = 'Введіть число!'
+        text.style.color = 'red'
+        return
+    }
+    if (yearValue % 4 === 0) {
+        text.textContent = 'Ви народилися у високосний рік!'
+        text.style.color = 'green'
+    // }else if (){
+    //     // check for 100
+    // }else if (){
+        // check for 400
+    }else{
+        text.textContent = 'Ви народилися не у високосний рік!'
+        text.style.color = 'red'
+    }
+    form.reset()
+}
