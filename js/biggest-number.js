1572761b08091debb1126a01ba94a7ad0194a387
Bogdan3690 <dblevko@gmail.com>
2025-12-29 15:22:47 -0700

    alo

--- js/biggest-number.js
+++ js/biggest-number.js
@@ -1,20 +1,33 @@
-const input = document.querySelectorAll('.js-biggest-number')
-const text = document.querySelector('.js-biggest-number-text')
-console.log(input);
+const input = document.querySelectorAll('.js-biggest-number');
+const text = document.querySelector('.js-biggest-number-text');
 
-input.addEventListener('input', onInput)
+input.forEach(element =>{
+    element.addEventListener('input', ()=>{
+        let a = Number(input[0].value);
+        let b = Number(input[1].value);
+        let c = Number(input[2].value);
+        let biggest = a;
+        if(b > biggest){
+            biggest = b;
+        }
+        if(c > biggest){
+            biggest = c;
+        }
+        text.textContent = `Найбільше число, яке ви ввели - ${biggest}`;
+    });
+});
 
-function onInput(ev){
-    ev.preventDefault()
-    let value = ev.target.value
-    console.log(value);
-    const numberValue = Number.parseInt(value)
-    if (Number.isNaN(numberValue)) {
-        text.textContent = 'Введіть число!'
-        text.style.color = 'red'
-        return
-    }
+// function onInput(ev){
+//     ev.preventDefault()
+//     let value = ev.target.value
+//     console.log(value);
+//     const numberValue = Number.parseInt(value)
+//     if (Number.isNaN(numberValue)) {
+//         text.textContent = 'Введіть число!'
+//         text.style.color = 'red'
+//         return
+//     }
     
-    // let biggestNumber = Math.max(numberValue)
-    form.reset()
-}
+//     // let biggestNumber = Math.max(numberValue)
+//     form.reset()
+// }
