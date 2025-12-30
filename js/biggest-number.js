cbbfe8a69e9327499bb4ebecca5598d3823eab4c
Bogdan3690 <dblevko@gmail.com>
2025-12-30 13:33:03 -0700

    add black theme

--- js/biggest-number.js
+++ js/biggest-number.js
@@ -1,25 +1,22 @@
-const inputs = document.querySelectorAll('.js-biggest-number');
-const text = document.querySelector('.js-biggest-number-text');
-const result = text.querySelector('.js-result');
+const inputs = document.querySelectorAll(".js-biggest-number");
+const text = document.querySelector(".js-biggest-number-text");
+const result = text.querySelector(".js-result");
 console.log(result);
 
+inputs.forEach((element) => element.addEventListener("input", onInput));
 
-inputs.forEach(element =>(element.addEventListener('input', onInput)))
+function onInput(ev) {
+  let a = Number(inputs[0].value);
+  let b = Number(inputs[1].value);
+  let c = Number(inputs[2].value);
+  if (Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
+    text.textContent = "Введіть число!";
+    text.style.color = "red";
+    return;
+  }
 
-function onInput(ev){
-        let a = Number(inputs[0].value);
-        let b = Number(inputs[1].value);
-        let c = Number(inputs[2].value);
-    if (Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)){
-        text.textContent = 'Введіть число!'
-        text.style.color = 'red'
-        return
-    }
+  let biggestNumber = Math.max(a, b, c);
+  console.log(biggestNumber);
 
-
-    let biggestNumber = Math.max(a, b, c)
-    console.log(biggestNumber);
-
-    result.textContent = biggestNumber
+  result.textContent = biggestNumber;
 }
-    
---
+++ js/theme-change.js
@@ -0,0 +1,24 @@
+const themeBtn = document.querySelector(".switch-theme-btn");
+const whiteThemeImg = themeBtn.querySelector(".img-white-theme")
+const blackThemeImg = themeBtn.querySelector(".img-black-theme")
+console.log(blackThemeImg);
+
+themeBtn.addEventListener('click', onToogleBtnClick)
+
+let darkTheme = false
+
+function onToogleBtnClick(ev){
+    darkTheme = !darkTheme
+    console.log(darkTheme);
+    if (darkTheme === true) {
+        blackThemeImg.classList.remove('hidden')
+        whiteThemeImg.classList.add('hidden')
+        document.documentElement.style.setProperty('--text-color', '#ffffff9e')
+        document.documentElement.style.setProperty('--main-bg-color', '#000000ba')
+    }else{
+        whiteThemeImg.classList.remove('hidden')
+        blackThemeImg.classList.add('hidden')
+        document.documentElement.style.setProperty('--text-color', 'black')
+        document.documentElement.style.setProperty('--main-bg-color', 'white')
+    }
+}
